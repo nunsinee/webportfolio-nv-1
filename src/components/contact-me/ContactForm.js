3105b2af921cdea0bf6d56c622343b9541749258
Nunsinee Viken <71260513+nunsinee@users.noreply.github.com>
2022-07-15 22:30:25 +0200

    update contact form

--- src/components/contact-me/ContactForm.js
+++ src/components/contact-me/ContactForm.js
@@ -1,21 +1,69 @@
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import { Alert, Col } from "react-bootstrap";
 
 export default function ContactForm() {
+	const form = useRef();
+	const [sending, setSending] = useState("");
+
+	const sendEmail = (e) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				"service_6noogkt",
+				"template_xf6zs66",
+				form.current,
+				"user_aWHytTFfrwu1LGrf2zQBz"
+			)
+			.then(
+				(result) => {
+					console.log(result.text);
+					console.log("message sent!");
+					setSending(true);
+				},
+				(error) => {
+					console.log(error.text);
+				}
+			);
+	};
+
 	return (
 		<>
 			<Form>
+				{sending && (
+					<Col sm>
+						<Alert variant="info">
+							<Alert.Heading>
+								Thank you for your message!
+							</Alert.Heading>
+						</Alert>
+					</Col>
+				)}
+			</Form>
+			<Form ref={form} onSubmit={sendEmail}>
 				<Form.Group className="mb-3">
-					<Form.Control type="text" placeholder="Name" />
+					<Form.Control
+						type="text"
+						name="user_name"
+						placeholder="Name"
+					/>
 				</Form.Group>
 
 				<Form.Group className="mb-3">
-					<Form.Control type="email" placeholder="Email" />
+					<Form.Control
+						type="email"
+						name="user_email"
+						placeholder="Email"
+					/>
 				</Form.Group>
 
 				<Form.Group className="mb-3">
 					<Form.Control
 						as="textarea"
+						name="message"
 						rows={3}
 						placeholder="Write your message here"
 					/>
@@ -24,9 +72,10 @@ export default function ContactForm() {
 				<Button
 					variant="primary"
 					type="submit"
+					value="Send"
 					className="btn btn-dark submit"
 				>
-					Submit
+					Send
 				</Button>
 			</Form>
 		</>
